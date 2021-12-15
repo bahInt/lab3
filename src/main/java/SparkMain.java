6ce830fd1d824caceb7c5bd2e53ec25da36bfe30
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-16 01:20:02 +0300

    Scripted auto-commit on change (2021-12-16 01:20:02) by gitwatch.sh

--- src/main/java/SparkMain.java
+++ src/main/java/SparkMain.java
@@ -1,6 +1,7 @@
 public class SparkMain {
     public static void main() {
-        SparkConf conf = new SparkConf().setAppName();
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
 
     }
 }
