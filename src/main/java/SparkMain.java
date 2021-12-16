d33588e9b7abac2a553004d05f564f0153b464a1
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-16 16:15:35 +0300

    Scripted auto-commit on change (2021-12-16 16:15:35) by gitwatch.sh

--- src/main/java/SparkMain.java
+++ src/main/java/SparkMain.java
@@ -4,10 +4,11 @@ import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
 public class SparkMain {
-    public static void main() {
+    public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
+        if(args.length != 3){}
         JavaRDD<String> airportsFile = sc.textFile("L_AIRPORT_ID.csv");
         JavaRDD<String> flightsFile = sc.textFile("664600583_T_ONTIME_sample.csv");
 
