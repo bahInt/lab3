579b9d60242c99fb27dbcbaaf3571a3c4b2927f4
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-16 16:18:56 +0300

    Scripted auto-commit on change (2021-12-16 16:18:56) by gitwatch.sh

--- src/main/java/SparkMain.java
+++ src/main/java/SparkMain.java
@@ -1,9 +1,9 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
-import java.util.Map;
 
 public class SparkMain {
     public static void main(String[] args) {
@@ -17,8 +17,8 @@ public class SparkMain {
         JavaRDD<String> airportsFile = sc.textFile("L_AIRPORT_ID.csv");
         JavaRDD<String> flightsFile = sc.textFile("664600583_T_ONTIME_sample.csv");
 
-        Map<Integer, String> airportsMapToPair = airportsFile.mapToPair(
-                line -> new Tuple2<>(airportID, airportName);
+        JavaPairRDD<Integer, String> airportsMapToPair = airportsFile.mapToPair(
+                line -> new Tuple2<>(airportID, airportName)
         )
 
     }
