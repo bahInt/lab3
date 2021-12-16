a05826fed871a722a24d0bc6ef9b1981c0682be4
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-17 00:04:41 +0300

    Scripted auto-commit on change (2021-12-17 00:04:41) by gitwatch.sh

--- src/main/java/AirportSerializable.java
+++ src/main/java/AirportSerializable.java
@@ -4,5 +4,9 @@ public class AirportSerializable implements Serializable {
     private int departureAirportID;
     private int destinationAirportID;
     private float delay;
-    boolean
+    private boolean isFlightCancelled;
+
+    public AirportSerializable(int departureAirportID, int destinationAirportID, ) {
+
+    }
 }
