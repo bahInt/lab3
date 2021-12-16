2e70ce928f9e3b86f796a2cd58671064c2c2918d
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-16 22:03:01 +0300

    Scripted auto-commit on change (2021-12-16 22:03:01) by gitwatch.sh

--- src/main/java/SparkMain.java
+++ src/main/java/SparkMain.java
@@ -48,7 +48,8 @@ public class SparkMain {
             String[] flightsDataTable = line.split(COMMA);
             int departureAirportID = Integer.parseInt(flightsDataTable[ORIGIN_AIRPORT_ID]);
             int destinationAirportID = Integer.parseInt(flightsDataTable[DEST_AIRPORT_ID]);
-            float delay = getDelay(line);
+            float delay = getDelay(flightsDataTable[DELAY_ID]);
+            
         });
     }
 }
