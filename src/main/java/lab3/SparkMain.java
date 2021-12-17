4905b86741a52111dde640653775425cedc51721
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-17 16:40:56 +0300

    Scripted auto-commit on change (2021-12-17 16:40:56) by gitwatch.sh

--- src/main/java/lab3/SparkMain.java
+++ src/main/java/lab3/SparkMain.java
@@ -32,6 +32,7 @@ public class SparkMain {
         Map<Integer, String> stringAirportDataMap = makeAirportsPair(airportsFile).collectAsMap();
         final Broadcast<Map<Integer, String>> airportsBroadcasted = sc.broadcast(stringAirportDataMap);
         JavaRDD<String> resultStatistics = mapStatistics(reducedFlightsData, airportsBroadcasted);
+        resultStatistics.saveAsTextFile();
     }
 
 
