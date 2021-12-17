a10481a1eb3c9ac09ee76dd507c9ef7445d03ef3
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-17 16:27:53 +0300

    Scripted auto-commit on change (2021-12-17 16:27:53) by gitwatch.sh

--- src/main/java/lab3/SparkMain.java
+++ src/main/java/lab3/SparkMain.java
@@ -88,6 +88,7 @@ public class SparkMain {
             Tuple2<Integer, Integer> key = flight._1();
             String departureAirport = departureDestinationAirports.get(key._1);
             String destinationAirport = departureDestinationAirports.get(key._2);
+            
             }
         )
     }
