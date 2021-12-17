31b44031bb084f00484ae95e8f4a7c1c000dc83a
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-17 15:00:56 +0300

    Scripted auto-commit on change (2021-12-17 15:00:56) by gitwatch.sh

--- src/main/java/lab3/FlightSerializable.java
+++ src/main/java/lab3/FlightSerializable.java
@@ -5,7 +5,8 @@ import java.io.Serializable;
 public class FlightSerializable implements Serializable {
     private float maxDelayTime;
     private float delayedFlights;
-    private float
+    private float cancelledFlights;
+    private int amountOfFlights;
 
     public FlightSerializable(float maxDelayTime)
 }
